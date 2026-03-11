e12fe7a2c7337327f60db04eafe108ea08af8f45
Nguyen Trong Nghia-2074 <nghialop10a1@gmail.com>
2026-03-11 08:32:59 +0700

    Hoan thanh ham sum va sub trong Step 03

---
+++ main.py
@@ -0,0 +1,13 @@
+# sum a,b
+def sum(a, b):
+    return a + b
+
+# sub a, b
+def sub(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    a = 10
+    b = 20
+    print("sum of a and b is ", sum(a, b))
+    print("sub of a and b is ", sub(a, b))
